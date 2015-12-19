1b06d5ba3a3d195fe699d017e11fac832350fe8f
tkmagesh <tkmagesh77@gmail.com>
2015-12-19 11:44:12 +0530

    BugTracker - Server Communication

--- Day-03/03-Async/AsyncOps.js
+++ Day-03/03-Async/AsyncOps.js
@@ -29,3 +29,49 @@ function addAsyncClient(x,y){
         console.log("[SC] result = ", addResult);
     });
 }
+
+/* Async - Events */
+var asyncAdder = (function(){
+    var _callbacks = [];
+    return {
+        add : function(x,y){
+            console.log("[SP] processing ", x , " and ", y);
+            setTimeout(function(){
+                var result = x + y;
+                console.log("[SP] returning result");
+                _callbacks.forEach(function(callback){
+                    callback(result);
+                });
+            },3000);
+        },
+        subscribeForResult : function(callback){
+            _callbacks.push(callback);
+        }
+    }
+})();
+
+/*asyncAdder.subscribeForResult(function(result){
+   console.log("[SC] result = ", result);
+});
+asyncAdder.add(100,200);*/
+
+/* Async - Promise*/
+
+function addAsyncPromise(x,y){
+    var promise = new Promise(function(resolve, reject){
+        console.log("[SP] processing ", x , " and ", y);
+        setTimeout(function(){
+            var result = x + y;
+            console.log("[SP] returning result");
+            resolve(result);
+        },3000);
+    });
+    return promise;
+}
+
+
+
+
+
+
+
